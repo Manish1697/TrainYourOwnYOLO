8d971d63662900490d7e06b77063aa76c5678ebb
Manish1697 <84699802+Manish1697@users.noreply.github.com>
2021-06-02 09:47:14 +0530

    Update Train_YOLO.py

--- 2_Training/Train_YOLO.py
+++ 2_Training/Train_YOLO.py
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--epochs",
         type=int,
-        default=5,
-        help="Number of epochs for training last layers and number of epochs for fine-tuning layers. Default is 51.",
+        default=50,
+        help="Number of epochs for training last layers and number of epochs for fine-tuning layers. Default is 50.",
     )
     parser.add_argument(
         "--warnings",
